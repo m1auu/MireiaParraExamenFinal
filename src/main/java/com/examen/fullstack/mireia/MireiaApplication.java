d37f4f3801d9ac8d8724665db5bc5b883f5f28f4
MireiaParra <miahigurashi02@gmail.com>
2023-11-27 13:09:16 +0100

    Primer commit cfg.

---
+++ src/main/java/com/examen/fullstack/mireia/MireiaApplication.java
@@ -0,0 +1,13 @@
+package com.examen.fullstack.mireia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MireiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MireiaApplication.class, args);
+	}
+
+}
